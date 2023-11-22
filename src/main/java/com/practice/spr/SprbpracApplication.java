f33eae6e06a10d353c1c1187b414353d84377c34
ChongWei905 <nemoweich@163.com>
2023-11-23 00:26:06 +0800

    feat: first commit

---
+++ src/main/java/com/practice/spr/SprbpracApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprbpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprbpracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/spr/SprbpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprbpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
